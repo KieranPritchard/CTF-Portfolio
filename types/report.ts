b04b07646e8b37377a9de5a4962d64d1a00d1e27
Kieran Pritchard <KieranPritchard06@gmail.com>
2026-06-10 20:34:27 +0100

    created one ./types/

---
+++ types/report.ts
@@ -0,0 +1,2 @@
+// represents the categories a project can belong to
+export type ReportCategory = "web-application" | "linux" | "windows" | "password-cracking" | "database-exploitation" | "osint" | "cryptography"
