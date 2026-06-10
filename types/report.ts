f35c9b7a24fbb940221911f5df2b6dfcc5505614
Kieran Pritchard <KieranPritchard06@gmail.com>
2026-06-10 20:38:44 +0100

    added report frontmatter

--- types/report.ts
+++ types/report.ts
@@ -1,2 +1,13 @@
 // represents the categories a project can belong to
 export type ReportCategory = "web-application" | "linux" | "windows" | "password-cracking" | "database-exploitation" | "osint" | "cryptography"
+
+// Report frontmatter extracted from markdown file
+export type ReportFrontmatter = {
+    title: string
+    slug: string
+    category: ReportCategory
+    description: string
+    date: string
+    coverImage?: string
+    coverAlt?: string
+}
