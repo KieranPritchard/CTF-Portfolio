867572e5c9c87157403de15dfbc00a09a523d7cc
Kieran Pritchard <KieranPritchard06@gmail.com>
2026-06-10 20:21:33 +0100

    renamed files

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import AboutSection from "@/components/home/AboutSection"
-import Welcome from "@/components/home/Welcome"
+import AboutSection from "@/components/Home/AboutSection"
+import Welcome from "@/components/Home/Welcome"
 
 export default function Page() {
   return (
