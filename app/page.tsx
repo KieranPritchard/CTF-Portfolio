e6ad4a08bef436773308091d8ae8aeb85e7ab3c1
Kieran Pritchard <KieranPritchard06@gmail.com>
2026-06-21 17:10:52 +0100

    changed file name

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import AboutSection from "@/components/Home/AboutSection"
-import Welcome from "@/components/Home/Welcome"
+import Welcome from "@/components/Home/WelcomeSection"
 
 export default function Page() {
   return (
